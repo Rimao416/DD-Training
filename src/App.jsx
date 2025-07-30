4b26bf0a4ff716322d94dcddd3214b59a4628779
Rimao416 <kayumbaomari27@gmail.com>
2025-07-30 22:12:08 +0200

    Ajout des composants

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,24 @@ import "./App.css"
 
 
 
-function App() {
+const Presentation=({nom})=>{
+  return(
+    <div className="presentation">
+  <h1>{nom}</h1>
+  <p>Bonsoir {nom}, deja felicitation d'avoir fini cette formation full Stack, je vous souhaite le meilleur</p>
+</div>
+  )
+}
+const nom=["Mr Author","Mr Louad","Mr Adlès","Mr Serge","Mr Christian","Mr Sinai","Mr Medy","Mr Oracle"]
+for (let index = 0; index < nom.length; index++) {
+  const element = nom[index];
+  console.log(element)
   
-  return (
+}
+function App() {
+   return (
     <>
-<Header/>
-   
+{nom.map((element,index)=><Presentation key={index} nom={element}/>)}
     </>
   )
 }
