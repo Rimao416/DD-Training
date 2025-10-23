24e0b42a1dbecf44a4fddfe1b295c0bdbf6b806e
Rimao416 <kayumbaomari27@gmail.com>
2025-10-23 22:46:51 +0200

    :rocket: Exercice Django

--- urls.py
+++ urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dire_bonjour, name='bonjour'),
+    path('alassane', views.dire_bonjour, name='bonjour'),
 ]
--- views.py
+++ views.py
@@ -2,7 +2,15 @@ from django.shortcuts import render
 from datetime import datetime
 
 def dire_bonjour(request):
-    contexte = {
-        'heure_actuelle': datetime.now().strftime('%H:%M:%S')
+   
+    personnes = [
+        {'nom': "Omari", 'age': 26, 'ville': 'Tunisie'},
+        {'nom': "Alassane", 'age': 45, 'ville': 'Côte d’Ivoire'},
+        {'nom': "HP", 'age': 36, 'ville': 'Mali'},
+        {'nom': "Author", 'age': 33, 'ville': 'Togo'},
+        {'nom': "Franck", 'age': 80, 'ville': 'Somalie'},
+    ]
+    contexte={
+        'personnes':personnes
     }
-    return render(request, 'hello/bonjour.html', contexte)
+    return render(request, 'hello/bonjour.html',contexte)
