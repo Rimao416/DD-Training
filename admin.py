ab22b4c72a7ec5589deeb6f08c964cae612292af
Rimao416 <kayumbaomari27@gmail.com>
2025-10-30 22:24:40 +0100

    :rocket: Amélioration de la vue

--- admin.py
+++ admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Article
+from .models import Commentaire
+from .models import Categorie
 
-# Register your models here.
+class CommentaireInline(admin.TabularInline):
+    model = Commentaire
+    extra = 1  # Nombre de formulaires vides
+    fields = ('auteur', 'email', 'contenu', 'approuve')
+    
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('titre', 'auteur', 'statut', 'vues')
+    search_fields=('titre','contenu')
+    list_editable=('statut',)
+    prepopulated_fields={'slug':('titre',)}
+    inlines=[CommentaireInline]
+
+
+
+
+
+admin.site.register(Article,ArticleAdmin)
+admin.site.register(Categorie)
+admin.site.register(Commentaire)
